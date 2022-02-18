53e7b40bb0a0bb853e92c69456fbf11aefb18173
Anand0616 <91175530+Anand0616@users.noreply.github.com>
2022-02-18 10:48:46 +0800

    Create types.cpp

---
+++ week_1/types.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //integer
+    int a = 7;
+    cout << a << "\n";
+
+    //double 
+    double b = 7.007097;
+    cout << b << "\n";
+
+    //strings
+    string c = "Hello";
+    cout << c << "\n";
+
+
+    //boolean
+    bool d = true;
+    cout << d << "\n";
+
+
+}
